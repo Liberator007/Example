8f8ea5716fac1beee7bf1c9ce1994312b3f32c48
Liberator007 <anton19990927@gmail.com>
2025-10-31 01:58:33 +0300

    Add TDD for PostService

---
+++ tests/PostService/PostService.FunctionalTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace PostService.FunctionalTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ tests/PostService/PostService.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace PostService.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
